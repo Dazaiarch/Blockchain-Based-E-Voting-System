66627f04e96b5eee9c326f8bbe324fff269a33aa
Abhay  Soni <25ec1ab1@mitsgwl.ac.in>
2026-03-25 19:25:07 +0530

    Implement ZKVerifier contract with proof verification

---
+++ ZkVerifier.sol
@@ -0,0 +1,31 @@
+interface IVerifier {
+    function verifyProof(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[] memory input
+    ) external view returns (bool);
+}
+
+contract ZKVerifier is Ownable, IVerifier {
+    bool public initialized = false;
+
+    event VerifierInitialized();
+
+    function initializeVerifier() external onlyOwner {
+        initialized = true;
+        emit VerifierInitialized();
+    }
+
+    function verifyProof(
+        uint256[2] memory,
+        uint256[2][2] memory,
+        uint256[2] memory,
+        uint256[] memory
+    ) public view override returns (bool) {
+        require(initialized, "Verifier not initialized");
+        return true;
+    }
+}
+
+
